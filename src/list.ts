95de1669be177ffdb7184a466d835bc51d19e335
yueb <nobodyplay@yeah.net>
2025-04-02 07:41:22 +0800

    doc: list函数

--- src/list.ts
+++ src/list.ts
@@ -1,8 +1,23 @@
 type IterType<T = unknown> = Iterable<T> | ArrayLike<T>;
-
 type MapMethod<T = unknown, U = unknown> = (item: T, index: number) => U;
 
-/** 类型安全地创建数组 */
+/**
+ * 类型安全的创建数组的工厂函数，支持多种参数形式
+ *
+ * @example
+ * // 无参数调用(如用ts时最好传入泛型参数以保障类型安全)
+ * list<number>(); // => number[]
+ *
+ * @example
+ * // 转换类数组对象
+ * list(new Set([1, 2, 3])); // => [1, 2, 3]
+ *
+ * @example
+ * // 带映射函数
+ * list([1, 2, 3], x => x * 2); // => [2, 4, 6]
+ *
+ * @throws {Error} 当参数超过两个时抛出错误
+ */
 function list<T = unknown>(): T[];
 function list<T = unknown>(iter: IterType<T>): T[];
 function list<T = unknown, U = unknown>(iter: IterType<T>, mapMethod: MapMethod<T, U>): U[];
