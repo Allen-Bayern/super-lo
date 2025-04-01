5d4f4505306aae97ef4fc10f6df44bddc1e6b91e
yueb <nobodyplay@yeah.net>
2025-04-02 07:43:22 +0800

    feat: 增加list默认导出

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as deepFreeze } from './deep-freeze';
 export { default as divmod } from './divmod';
 export { enumerate, enumerateArray } from './enumerate';
 export { default as getattr } from './getattr';
+export { default as list } from './list';
 export { default as safeObjectKeys } from './safe-object-keys';
 export { default as setattr } from './setattr';
 export { default as simpleClassName } from './simple-classname';
